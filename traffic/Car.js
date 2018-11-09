96b1b0e89dfcc1a6b26c1b2487c54f1b19ef4532
Wojciech Kaspera <wojciech.kaspera@gmail.com>
2018-11-09 02:34:29 +0100

    initial working demo

---
+++ traffic/Car.js
@@ -0,0 +1,48 @@
+class Car {
+    constructor(name, carNode, direction = 0, speed = 0, positionX = 0, positionY = 0, origin) {
+        this.name = name;
+        this.direction = direction;
+        this.speed = speed;
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.initialPositionX = positionX;
+        this.initialPositionY = positionY;
+        this.carNode = carNode;
+        this.enginOn = false;
+        this.stopInterval = {};
+        this.origin = origin;
+        this.destination = '';
+        this.blinkInterval = {};
+        this.lightsPassed = false;
+    }
+    // movement
+    accelerate(howMuch = 1) {
+        if (this.speed === 0) {
+            this.enginOn = true;
+        }
+        if(this.speed < 5) {
+            this.speed += howMuch;
+        }
+    }
+    break(howMuch = 1) {
+        if (this.speed > howMuch) {
+            this.speed -= howMuch;
+        } else {
+            this.stop();
+        }
+    }
+    go() {
+        this.enginOn = true;
+        this.movement = setInterval(() => {
+            this.positionX += Math.cos((this.direction + 90) / 360 * 2 * Math.PI) * this.speed;
+            this.positionY -= Math.sin((this.direction + 90) / 360 * 2 * Math.PI) * this.speed;
+            this.carNode.style.left = this.positionX + `px`;
+            this.carNode.style.top = this.positionY + `px`;
+        }, 10);
+    }
+    stop() {
+        clearInterval(this.movement);
+        this.speed = 0;
+        this.enginOn = false;
+    }
+}
---
+++ traffic/main.js
@@ -0,0 +1,416 @@
+const config = {
+    initialPositions: {
+        south: {
+            X: 272,
+            Y: 550
+        },
+        west: {
+            X: -50,
+            Y: 272
+        },
+        north: {
+            X: 228,
+            Y: -50
+        },
+        east: {
+            X: 550,
+            Y: 228
+        }
+    },
+    carLength: 30,
+    defaultSpeed: 1,
+    middle: 250
+};
+
+//TODO obczaić auta z różnych stron
+//TODO zwalnianie i zatrzymywanie się samochodów jeśli wyczają, że przed nimi jest jakiś samochód
+//TODO zatrzymywanie się samochodu, jeżeli wyczai, że nie ma pierwszeństwa
+//TODO view - więcej modeli aut, dopasowane do ustawień etc.
+//TODO code refactoring - żeby ładniejszy był
+
+const cars = {};
+const stopObservers = {};
+const removingObservers = {};
+const turnObservers = {};
+const startBtn = document.getElementById('startBtn');
+const stopBtn = document.getElementById('stopBtn');
+const changeLightBtn = document.getElementById('changeLightBtn');
+const lights = {
+    sLight: {
+        position: 310,
+        greenLight: true
+    },
+    wLight: {
+        position: 190,
+        greenLight: false
+    },
+    nLight: {
+        position: 190,
+        greenLight: true
+    },
+    eLight: {
+        position: 310,
+        greenLight: false
+    },
+
+};
+const lightNodes = {
+    sLight: document.getElementById('sLights'),
+    wLight: document.getElementById('wLights'),
+    nLight: document.getElementById('nLights'),
+    eLight: document.getElementById('eLights')
+};
+
+// setInterval(addNewCar, 1500);
+lights.sLight.greenLight ? lightNodes.sLight.children[1].style.backgroundColor = 'green' : lightNodes.sLight.children[0].style.backgroundColor = 'red';
+lights.wLight.greenLight ? lightNodes.wLight.children[1].style.backgroundColor = 'green' : lightNodes.wLight.children[0].style.backgroundColor = 'red';
+lights.nLight.greenLight ? lightNodes.nLight.children[1].style.backgroundColor = 'green' : lightNodes.nLight.children[0].style.backgroundColor = 'red';
+lights.eLight.greenLight ? lightNodes.eLight.children[1].style.backgroundColor = 'green' : lightNodes.eLight.children[0].style.backgroundColor = 'red';
+
+changeLightBtn.addEventListener('click', () => {
+    for (let key in lights) {
+        changeLight(lights[key], lightNodes[key]);
+    }
+});
+
+startBtn.addEventListener('click', addNewCar);
+
+stopBtn.addEventListener('click', () => {
+    stop(cars[0], 70);
+});
+
+function addNewCar() {
+    const name = `car${Date.now()}`;
+    const origin = getRandomOrigin();
+    let destination = getRandomDirection();
+    //empty string for straight
+    const newCar = createNewCar(name, origin, 'car1.png');
+    newCar.destination = destination;
+    addObserver(newCar, origin);
+    cars[newCar.name] = newCar;
+    if (origin === 'south') {
+        newCar.imgRotate = newCar.direction + 90;
+    } else if (origin === 'west') {
+        newCar.imgRotate = newCar.direction - 90;
+    } else if (origin === 'north') {
+        newCar.imgRotate = newCar.direction + 90;
+    } else if (origin === 'east') {
+        newCar.imgRotate = newCar.direction - 90;
+    }
+    newCar.carNode.style.transform = `rotate(${newCar.imgRotate}deg)`;
+    start(newCar, config.defaultSpeed);
+    if (destination) {
+        setTimeout(() => {
+            prepareToTurn(newCar, destination);
+            // turn(newCar, destination);
+        }, 1200);
+    }
+}
+
+function changeLight(light, lightNode) {
+    if (light.greenLight) {
+        lightNode.children[1].style.backgroundColor = '#636363';
+        lightNode.children[0].style.backgroundColor = 'red';
+    } else {
+        lightNode.children[1].style.backgroundColor = 'green';
+        lightNode.children[0].style.backgroundColor = '#636363';
+        for (let key in cars) {
+            const car = cars[key];
+            if (car.origin[0] === lightNode.id[0] && !car.lightsPassed) {
+                if (!car.enginOn) {
+                    startSlow(car, config.defaultSpeed);
+                } else {
+                    clearInterval(car.stopInterval);
+                    const speedDifference = config.defaultSpeed - car.speed;
+                    setTimeout(() => {
+                        car.accelerate(speedDifference / 3);
+                    }, 50);
+                    setTimeout(() => {
+                        car.accelerate(speedDifference / 3);
+                    }, 150);
+                    setTimeout(() => {
+                        car.accelerate(speedDifference / 3);
+                    }, 250);
+                }   
+            }
+        }
+    }
+    light.greenLight = !light.greenLight;
+}
+
+function addObserver(car, origin) {
+    const light = lights[`${origin[0]}Light`];
+    let carPosition;
+    let lightPosition;
+    stopObservers[car.name] = new MutationObserver(() => {
+        switch (car.origin) {
+            case 'north':
+                carPosition = car.positionY;
+                lightPosition = lights.nLight.position;
+                break;
+            case 'south':
+                carPosition = car.positionY;
+                lightPosition = lights.sLight.position;
+                break;
+            case 'east':
+                carPosition = car.positionX;
+                lightPosition = lights.eLight.position;
+                break;
+            case 'west':
+                carPosition = car.positionX;
+                lightPosition = lights.wLight.position;
+                break;
+        }
+        let distanceFromLight = Math.abs(carPosition - lightPosition);
+        if (!light.greenLight && distanceFromLight < 100) {
+            stop(car, distanceFromLight);
+            stopObservers[car.name].disconnect();
+        }
+        if (distanceFromLight < 5) {
+            car.lightsPassed = true;
+            stopObservers[car.name].disconnect();
+        }
+    });
+    removingObservers[car.name] = new MutationObserver(() => {
+        if (car.positionX < -60 || car.positionX > 560 || car.positionY < -60 || car.positionY > 560) {
+            removingObservers[car.name].disconnect();
+            removeCar(car);
+        }
+    });
+    if (car.destination) {
+        turnObservers[car.name] = new MutationObserver(() => {
+            let turningDistance;
+            if (car.destination === 'right') {
+                turningDistance = 55;
+            } else if (car.destination === 'left') {
+                turningDistance = 5;
+            }
+            switch (car.origin) {
+                case 'north':
+                    carPosition = car.positionY;
+                    break;
+                case 'south':
+                    carPosition = car.positionY;
+                    break;
+                case 'east':
+                    carPosition = car.positionX;
+                    break;
+                case 'west':
+                    carPosition = car.positionX;
+                    break;
+            }
+            if (Math.abs(carPosition - config.middle) < (turningDistance)) {
+                turn(car);
+                turnObservers[car.name].disconnect();
+            }
+        });
+    }
+}
+
+function createNewCar(name, origin, img) {
+    const crossroad = document.getElementById('crossroad');
+    const carImgNode = document.createElement('img');
+    carImgNode.classList.add('car-img');
+    carImgNode.setAttribute('src', `assets/${img}`);
+
+    const rightBlinkerNode = document.createElement('img');
+    rightBlinkerNode.classList.add('right-blinker');
+    rightBlinkerNode.setAttribute('src', 'assets/blinker.png');
+
+    const leftBlinkerNode = document.createElement('img');
+    leftBlinkerNode.classList.add('left-blinker');
+    leftBlinkerNode.setAttribute('src', 'assets/blinker.png');
+
+    const carNode = document.createElement('div');
+    carNode.classList.add('car');
+    carNode.setAttribute('id', name);
+
+    carNode.appendChild(carImgNode);
+    carNode.appendChild(rightBlinkerNode);
+    carNode.appendChild(leftBlinkerNode);
+    crossroad.appendChild(carNode);
+
+    const X = config.initialPositions[origin].X;
+    const Y = config.initialPositions[origin].Y;
+
+    carNode.style.left = `${X}px`;
+    carNode.style.top = `${Y}px`;
+
+    let direction;
+
+    switch (origin) {
+        case 'south':
+            direction = 0;
+            break;
+        case 'west':
+            direction = 270;
+            break;
+        case 'north':
+            direction = 180;
+            break;
+        case 'east':
+            direction = 90;
+            break;
+    }
+
+    return new Car(name, carNode, direction, 0, X, Y, origin);
+}
+
+function prepareToTurn(car, direction) {
+    car.blinkInterval = blink(car, direction);
+    const carSpeed = car.speed;
+    const turningTimeout = 1100;
+    setTimeout(() => {
+        car.break(carSpeed / 6)
+    }, turningTimeout);
+    setTimeout(() => {
+        car.break(carSpeed / 6)
+    }, turningTimeout + 200);
+    setTimeout(() => {
+        car.break(carSpeed / 6);
+    }, turningTimeout + 400);
+}
+
+function turn(car) {
+    const direction = car.destination;
+    let turningInterval;
+    const initialDirection = car.direction;
+    if (direction === 'right') {
+        turningInterval = setInterval(() => {
+            car.imgRotate++;
+            car.carNode.style.transform = `rotate(${car.imgRotate}deg)`;
+            car.direction--;
+            if (car.direction < initialDirection - 90) {
+                car.direction = initialDirection - 90;
+                car.carNode.style.transform = `rotate(${car.imgRotate}deg)`;
+                car.speed = config.defaultSpeed;
+                clearInterval(turningInterval);
+                clearInterval(car.blinkInterval);
+                car.carNode.getElementsByClassName(`${direction}-blinker`)[0].style.visibility = 'hidden';
+            }
+        }, 10);
+    } else if (direction === 'left') {
+        turningInterval = setInterval(() => {
+            car.imgRotate--;
+            car.carNode.style.transform = `rotate(${car.imgRotate}deg)`;
+            car.direction++;
+            if (car.direction > initialDirection + 90) {
+                car.direction = initialDirection + 90;
+                car.carNode.style.transform = `rotate(${car.imgRotate}deg)`;
+                car.speed = config.defaultSpeed;
+                clearInterval(turningInterval);
+                clearInterval(car.blinkInterval);
+                car.carNode.getElementsByClassName(`${direction}-blinker`)[0].style.visibility = 'hidden';
+            }
+        }, 10);
+    }
+}
+
+function blink(car, direction) {
+    const blinker = car.carNode.getElementsByClassName(`${direction}-blinker`)[0];
+    let blinkerVisibility = false;
+    return setInterval(() => {
+        if (blinkerVisibility) {
+            blinker.style.visibility = 'hidden';
+            blinkerVisibility = !blinkerVisibility;
+        } else {
+            blinker.style.visibility = 'visible';
+            blinkerVisibility = !blinkerVisibility;
+        }
+    }, 200);
+}
+
+function stop(car, inDistance) {
+    if (car.direction) {
+        inDistance -= 30;
+    }
+    if (inDistance < 25) {
+        console.log('hamuje z piskiem opon');
+    }
+    const speed = car.speed;
+    const interval = inDistance / 0.306 / speed;
+    car.stopInterval = setInterval(() => {
+        if (car.enginOn) {
+            car.break(speed / 5);
+        } else {
+            clearInterval(car.stopInterval);
+        }
+    }, interval);
+}
+
+function start(car, speed) {
+    stopObservers[car.name].observe(car.carNode, {attributes: true, childList: false, subtree: false});
+    removingObservers[car.name].observe(car.carNode, {attributes: true, childList: false, subtree: false});
+    if (car.destination) {
+        turnObservers[car.name].observe(car.carNode, {attributes: true, childList: false, subtree: false});
+    }
+    if (car.speed === 0) {
+        car.go();
+        setTimeout(() => {
+            car.accelerate(speed / 3);
+        }, 50);
+        setTimeout(() => {
+            car.accelerate(speed / 3);
+        }, 150);
+        setTimeout(() => {
+            car.accelerate(speed / 3);
+        }, 250);
+    } else {
+        console.log('car already running!');
+    }
+}
+
+function startSlow(car, speed) {
+    car.go();
+    setTimeout(() => {
+        car.accelerate(speed / 6);
+    }, 50);
+    setTimeout(() => {
+        car.accelerate(speed / 6);
+    }, 250);
+    setTimeout(() => {
+        car.accelerate(speed / 6);
+    }, 550);
+    setTimeout(() => {
+        car.accelerate(speed / 6);
+    }, 2500);
+    setTimeout(() => {
+        car.accelerate(speed / 6);
+    }, 3000);
+    setTimeout(() => {
+        car.accelerate(speed / 6);
+    }, 3500);
+
+}
+
+function getRandomOrigin() {
+    const randomNumber = Math.random();
+    if (randomNumber < 0.25) {
+        return 'south';
+    } else if (randomNumber < 0.5) {
+        return 'west';
+    } else if (randomNumber < 0.75) {
+        return 'north';
+    } else {
+        return 'east';
+    }
+}
+
+function getRandomDirection() {
+    const randomNumber = Math.random();
+    if (randomNumber < 0.33) {
+        return 'left';
+    } else if (randomNumber < 0.66) {
+        return ''
+    } else {
+        return 'right';
+    }
+}
+
+function removeCar(car) {
+    car.carNode.remove();
+    stopObservers[car.name].disconnect();
+    delete stopObservers[car.name];
+    delete cars[car.name];
+    delete car;
+}
